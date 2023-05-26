a42911bd968608cb6ddb15664bec9af636428f85
Leonid <leo.breydo@gmail.com>
2023-05-26 11:57:44 +0300

    Unit tests

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../b_checker/b_checker.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(verify("before ( middle []) after "), 1);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual(verify("---(++++)----"), 1);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Assert::AreEqual(verify(""), 1);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::AreEqual(verify(") ("), 0);
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			Assert::AreEqual(verify("<(   >)"), 0);
+		}
+		TEST_METHOD(TestMethod6)
+		{
+			Assert::AreEqual(verify("(  [  <>  ()  ]  <>  )"), 1);
+		}
+		TEST_METHOD(TestMethod7)
+		{
+			Assert::AreEqual(verify("   (      [)"), 0);
+		}
+	};
+}
